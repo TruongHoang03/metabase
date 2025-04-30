cf8643f32f9a11203a4cd008b2b22849582504b4
Raphael Krut-Landau <raphael.kl@gmail.com>
2025-04-30 07:30:07 -0400

    This introduces logic that validates content translation dictionaries when they are uploaded.
    A valid dictionary must have:
    
    * Three fields per row
    * A valid locale in the first column
    * One translation per string per locale (for example, you can only provide one translation of "Category" into German - you can't have two rows that begin "de,Category,")
    * Cells containing strings no longer than 255 characters

--- e2e/test/scenarios/admin/i18n/content-translation/constants.ts
+++ e2e/test/scenarios/admin/i18n/content-translation/constants.ts
@@ -30,3 +30,26 @@ export const nonAsciiFieldNames: DictionaryArray = [
   { locale: "zh-TW", msgid: "Title", msgstr: "标题" },
   { locale: "en", msgid: "Butterfly", msgstr: "🦋" },
 ];
+
+export const invalidLocaleXX = structuredClone(germanFieldNames);
+invalidLocaleXX[0].locale = "xx";
+
+export const multipleInvalidLocales = structuredClone(germanFieldNames);
+multipleInvalidLocales[0].locale = "ze";
+multipleInvalidLocales[3].locale = "qe";
+
+/** Maximum length of a cell in the upload */
+export const MAX_CELL_LENGTH = 255;
+
+export const longCSVCell = "a".repeat(MAX_CELL_LENGTH + 1);
+
+export const invalidLocaleAndInvalidRow = structuredClone(germanFieldNames);
+invalidLocaleAndInvalidRow[0].locale = "ze";
+invalidLocaleAndInvalidRow[3].msgstr = longCSVCell;
+
+export const stringTranslatedTwice = structuredClone(germanFieldNames);
+stringTranslatedTwice.push({
+  locale: "de",
+  msgid: "Title",
+  msgstr: "Überschrift",
+});
